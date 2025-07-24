b7de662b97a5cbd71e9a38099247f4f0cef7f8e7
Matteo Gianvenuti <mqtth3w@duck.com>
2025-07-24 16:04:14 +0200

    Add files via upload

--- KetoBot.js
+++ KetoBot.js
@@ -8,49 +8,46 @@
 
 
 export default {
-  async scheduled(controller, env, ctx) {
-      //console.log("cron processed");
+	async scheduled(controller, env, ctx) {
+		//console.log("cron processed");
 
-      const { users } = await env.db.prepare("SELECT id FROM users").all();
+		const { users } = await env.db.prepare("SELECT id FROM users").all();
 
-      if (users.lenght > 0) {
-        let breafast = await getDishByMeal(env, "breakfast");
-        let lunch = await getDishByMeal(env, "lunch");
-        let dinner = await getDishByMeal(env, "dinner");
+		if (users.lenght > 0) {
+			let breafast = await getDishByMeal(env, "breakfast");
+			let lunch = await getDishByMeal(env, "lunch");
+			let dinner = await getDishByMeal(env, "dinner");
 
-        let msg = `Breakfast:\n${breafast[0].name}\n${breafast[0].time}\n${breafast[0].portions}
-          \n${breafast[0].nutritionFacts}\n${breafast[0].ingredients}\n${breafast[0].receipe}`;/* + 
+			let msg = `Breakfast:\n${breafast[0].name}\n${breafast[0].time}\n${breafast[0].portions}
+				\n${breafast[0].nutritionFacts}\n${breafast[0].ingredients}\n${breafast[0].receipe}`;/* + 
           
-          `\n\nLunch(first):\n${lunch[0].name}\n${lunch[0].time}\n${lunch[0].portions}
-          \n${lunch[0].nutritionFacts}\n${lunch[0].ingredients}\n${lunch[0].receipe}` +
-          `\nLunch(second):\n${lunch[1].name}\n${lunch[1].time}\n${lunch[1].portions}
-          \n${lunch[1].nutritionFacts}\n${lunch[1].ingredients}\n${lunch[1].receipe}` +
-          `\nLunch(side):\n${lunch[2].name}\n${lunch[2].time}\n${lunch[2].portions}
-          \n${lunch[2].nutritionFacts}\n${lunch[2].ingredients}\n${lunch[2].receipe}` +
+				`\n\nLunch(first):\n${lunch[0].name}\n${lunch[0].time}\n${lunch[0].portions}
+				\n${lunch[0].nutritionFacts}\n${lunch[0].ingredients}\n${lunch[0].receipe}` +
+				`\nLunch(second):\n${lunch[1].name}\n${lunch[1].time}\n${lunch[1].portions}
+				\n${lunch[1].nutritionFacts}\n${lunch[1].ingredients}\n${lunch[1].receipe}` +
+				`\nLunch(side):\n${lunch[2].name}\n${lunch[2].time}\n${lunch[2].portions}
+				\n${lunch[2].nutritionFacts}\n${lunch[2].ingredients}\n${lunch[2].receipe}` +
 
-          `\n\nDinner(first):\n${dinner[0].name}\n${dinner[0].time}\n${dinner[0].portions}
-          \n${dinner[0].nutritionFacts}\n${dinner[0].ingredients}\n${dinner[0].receipe}` +
-          `\nDinner(second):\n${dinner[1].name}\n${dinner[1].time}\n${dinner[1].portions}
-          \n${dinner[1].nutritionFacts}\n${dinner[1].ingredients}\n${dinner[1].receipe}` +
-          `\Dinner(side):\n${dinner[2].name}\n${dinner[2].time}\n${dinner[2].portions}
-          \n${dinner[2].nutritionFacts}\n${dinner[2].ingredients}\n${dinner[2].receipe}`;*/
+				`\n\nDinner(first):\n${dinner[0].name}\n${dinner[0].time}\n${dinner[0].portions}
+				\n${dinner[0].nutritionFacts}\n${dinner[0].ingredients}\n${dinner[0].receipe}` +
+				`\nDinner(second):\n${dinner[1].name}\n${dinner[1].time}\n${dinner[1].portions}
+				\n${dinner[1].nutritionFacts}\n${dinner[1].ingredients}\n${dinner[1].receipe}` +
+				`\Dinner(side):\n${dinner[2].name}\n${dinner[2].time}\n${dinner[2].portions}
+				\n${dinner[2].nutritionFacts}\n${dinner[2].ingredients}\n${dinner[2].receipe}`;*/
 
-        let meals = [{"\n\nLunch(first):\n": lunch}, {"\nLunch(second):\n": lunch}, {"\nLunch(side):\n": lunch}, 
-        {"\n\nDinner(first):\n": dinner}, {"\nDinner(second):\n": dinner}, {"\nDinner(side):\n": dinner}]
+			let meals = [{"\n\nLunch(first):\n": lunch}, {"\nLunch(second):\n": lunch}, {"\nLunch(side):\n": lunch}, 
+				{"\n\nDinner(first):\n": dinner}, {"\nDinner(second):\n": dinner}, {"\nDinner(side):\n": dinner}]
         
-        let i = 0;
-        for (const [key, value] of Object.entries(meals)) {
-          if (i === 3) i = 0;
-          msg += `${key}${value[i].name}\n${value[i].time}\n${value[i].portions}
-          \n${value[i].nutritionFacts}\n${value[i].ingredients}\n${value[i].receipe}`;
-          i++;
-        }
-
+			let i = 0;
+			for (const [key, value] of Object.entries(meals)) {
+				if (i === 3) i = 0;
+				msg += `${key}${value[i].name}\n${value[i].time}\n${value[i].portions}
+				\n${value[i].nutritionFacts}\n${value[i].ingredients}\n${value[i].receipe}`;
+				i++;
+			}
         await sendBroadcastMessage(env, msg, users);
-      }
-
-
-  },
+		}
+	},
 
   async fetch(request, env, ctx) {
     const secret_token = request.headers.get("X-Telegram-Bot-Api-Secret-Token");
@@ -61,61 +58,60 @@ export default {
 			const payload = await request.json();
 			if ('message' in payload) {
 				const chatId = payload.message.chat.id.toString();
-        const name = payload.message.chat.name || "";
-        const username = payload.message.chat.username || ""
+				const name = payload.message.chat.first_name || "";
+				const username = payload.message.chat.username || ""
 				const text = payload.message.text || "";
 				const command = text.split(" ")[0];
 				//const args = text.substring(command.length).trim();
 				try {
-          if (command === "/start") {
-            const { result } = await env.db.prepare("SELECT * FROM users WHERE id = ?")
-              .bind(chatId).all();
-            await sendMessage(env, chatId, `result: ${result}`);
-            if (result.lenght > 0) {
-              await sendMessage(env, chatId, `Welcome back ${name}!`);
-            } else {
-              await env.db.prepare("INSERT INTO users VALUES (?, ?, ?)")
-              .bind(chatId, name, username).run();
-              await sendMessage(env, chatId, `Hi ${name}, welcome to the KetoBot!`);
-            }
-          } else if (text) await searchDishes(env, chatId, "/searchname", text);
-        } catch (err) { await sendMessage(env, chatId, `error: ${err}`);}
-      }
-    }
+					if (command === "/start") {
+						const { results } = await env.db.prepare("SELECT * FROM users WHERE id = ?")
+							.bind(chatId).all();
+						if (results[0]) {
+							await sendMessage(env, chatId, `Welcome back ${name}!`);
+						} else {
+							await env.db.prepare("INSERT INTO users VALUES (?, ?, ?, ?)")
+								.bind(chatId, name, username, "false").run();
+							await sendMessage(env, chatId, `Hi ${name}, welcome to the KetoBot!`);
+						}
+					} else if (text) await searchDishes(env, chatId, "/searchname", text);
+				} catch (err) { await sendMessage(env, chatId, `error: ${err}`);}
+			}
+		}	
     return new Response("OK", { status: 200 });
   },
 };
 
 
 async function getDish(env, field, fieldVal) {
-  let { result } = await env.db.prepare(
-    `SELECT * FROM dishes WHERE alreadyTaken LIKE 'false' AND ${field} LIKE ? LIMIT 1`)
-    .bind(fieldVal).all();
-  if (result.lenght > 0) {
-    await env.db.prepare("UPDATE dishes SET alreadyTaken = 'true' WHERE rowid = ?")
-      .bind(result[0].rowid).run();
-  } else {
-    await env.db.prepare(`UPDATE dishes SET alreadyTaken = 'false' WHERE ${field} = ?`)
-      .bind(fieldVal).run();
-    result = await env.db.prepare(`SELECT * FROM dishes WHERE alreadyTaken LIKE 'false' AND ${field} LIKE ? LIMIT 1`)
-      .bind(fieldVal).all();
-    await env.db.prepare("UPDATE dishes SET alreadyTaken = 'true' WHERE rowid = ?")
-    .bind(result[0].rowid).all();
-  }
-  return result;
+	let { result } = await env.db.prepare(
+		`SELECT * FROM dishes WHERE alreadyTaken LIKE 'false' AND ${field} LIKE ? LIMIT 1`)
+		.bind(fieldVal).all();
+	if (result.lenght > 0) {
+		await env.db.prepare("UPDATE dishes SET alreadyTaken = 'true' WHERE rowid = ?")
+		.bind(result[0].rowid).run();
+	} else {
+		await env.db.prepare(`UPDATE dishes SET alreadyTaken = 'false' WHERE ${field} = ?`)
+			.bind(fieldVal).run();
+		result = await env.db.prepare(`SELECT * FROM dishes WHERE alreadyTaken LIKE 'false' AND ${field} LIKE ? LIMIT 1`)
+			.bind(fieldVal).all();
+		await env.db.prepare("UPDATE dishes SET alreadyTaken = 'true' WHERE rowid = ?")
+			.bind(result[0].rowid).all();
+	}
+	return result;
 }
 
 
 async function getDishByMeal(env, meal) {
-  let result;
-  if (meal === "breakfast") {
-    result = await getDish(env, "meal", meal);
-  } else {
-    result = await getDish(env, "type", "first");
-    result.append(await getDish(env, "type", "second"));
-    result.append(await getDish(env, "type", "side"));
-  }
-  return result;
+	let result;
+	if (meal === "breakfast") {
+		result = await getDish(env, "meal", meal);
+	} else {
+		result = await getDish(env, "type", "first");
+		result.append(await getDish(env, "type", "second"));
+		result.append(await getDish(env, "type", "side"));
+	}
+	return result;
 };
 
 /**
@@ -127,12 +123,12 @@ async function getDishByMeal(env, meal) {
  * @returns {Promise<void>} - This function does not return a value.
  */
 async function sendMessage(env, chatId, text) {
-  const url = `https://api.telegram.org/bot${env.API_KEY}/sendMessage`;
-  await fetch(url, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ chat_id: chatId, text })
-  });
+	const url = `https://api.telegram.org/bot${env.API_KEY}/sendMessage`;
+	await fetch(url, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ chat_id: chatId, text })
+	});
 };
 
 /**
@@ -145,10 +141,10 @@ async function sendMessage(env, chatId, text) {
  * @returns {Promise<void>} - This function does not return a value.
  */
 async function sendBroadcastMessage(env, msg, users) {
-  for (const userId of users) {
-    await sendMessage(env, userId, msg);
-    await new Promise(resolve => setTimeout(resolve, 33)); // Avoid hitting rate limits (30 messages/second      
-  }
+	for (const userId of users) {
+		await sendMessage(env, userId, msg);
+		await new Promise(resolve => setTimeout(resolve, 33)); // Avoid hitting rate limits (30 messages/second      
+	}
 };
 
 /** 
@@ -162,10 +158,10 @@ async function sendBroadcastMessage(env, msg, users) {
  */
 async function searchDishes(env, chatId, command, data) {
 	const fieldMap = {
-			"/searchname": "name"
-		};
+		"/searchname": "name"
+	};
     const { results } = await env.db.prepare(`SELECT * FROM dishes WHERE ${fieldMap[command]} LIKE ?`)
-								.bind(`%${data}%`).all();
+		.bind(`%${data}%`).all();
     if (results.length === 0) return await sendMessage(env, chatId, `No data`);
     let total = 0;
 	let message = "";
@@ -179,8 +175,8 @@ async function searchDishes(env, chatId, command, data) {
 			`Ingredients: ${dish.ingredients}\n` +
 			`Recipe: ${dish.receipe}\n` +
 			`Type: ${dish.type}\n` +
-      `Category: ${dish.category}\n` +
-      `Already taken: ${dish.alreadyTaken}\n` +
+			`Category: ${dish.category}\n` +
+			`Already taken: ${dish.alreadyTaken}\n` +
 			`Meal: ${dish.meal}\n\n`;
 		if ((total % batchSize === 0) || (i === results.length - 1)) {
 			if (i === results.length - 1) {
@@ -190,5 +186,5 @@ async function searchDishes(env, chatId, command, data) {
 			await new Promise(resolve => setTimeout(resolve, 31));
 			message = ""; 
 		}
-  }
+	}
 };
